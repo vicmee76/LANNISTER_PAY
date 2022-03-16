269a790ff77a2deb9b543d49c76cec8565aad715
ComputerGuy <vicmee76@hotmail.com>
2022-03-16 13:30:41 +0100

    -- added route and controller files

--- app.js
+++ app.js
@@ -1,2 +1,13 @@
+require("dotenv").config();
 const express = require('express');
 const app = express();
+const port = process.env.S_PORT || 3000;
+
+
+app.get('/', (req, res) => {
+    res.send("Welcome");
+});
+
+app.listen(port, () => {
+    console.log('Running on port ' + port);
+})
--- config/server.js
+++
@@ -1,4 +0,0 @@
-const http = require('http');
-const port = process.env.PORT;
-const server = http.createServer();
-server.listen(port);
