227a886aa7c0d60d870d84ce42158e4fc0dfc42e
ComputerGuy <vicmee76@hotmail.com>
2022-03-19 21:06:58 +0100

    -- code clean up of un wanted files and folders

--- api/controllers/fees_controller.js
+++ api/controllers/fees_controller.js
@@ -1,6 +1,6 @@
 const helpers = require("../../helpers/response");
 const { _feesMiddleWearAction } = require("../middleware/fees_middleware");
-const { _feesService } = require("../services/fees_service");
+const FeesModel = require("../models/fees_model");
 
 const {
     _clearError,
@@ -10,31 +10,46 @@ const {
     compareEntityProperty
 } = require("../../helpers/validator");
 
-
-let _feesResult=[];
+let _feesResult = [];
 
 exports._feesController = async (req, res) => {
     try {
         _feesResult = null;
+
         const data = req.body;
         if (data === null || data.FeeConfigurationSpec === "")
             helpers._showError(res, 400, "Fees configuration spec cannot be null");
         let feesConfigSpec = data.FeeConfigurationSpec.split("\n");
-        _feesResult = await _feesMiddleWearAction(feesConfigSpec, res);
+        let _feesValidationResults = await _feesMiddleWearAction(feesConfigSpec, res);
 
         if (_sendError().length > 0) {
             helpers._showError(res, 500, _sendError());
         }
         else {
-            if (_feesResult.length > 0) {
-                let result = await _feesService(_feesResult);
-                console.log(result);
-                return res.status(200).json({
-                    "status": "ok"
-                });
+            if (_feesValidationResults.length > 0) {
+
+                await FeesModel.deleteMany();
+                await FeesModel.insertMany(_feesValidationResults)
+                    .then(result => {
+                        if (result.length > 0) {
+                            _feesResult = result;
+                            res.status(200).json({
+                                "status": "ok"
+                            });
+                        }
+                        else {
+                            res.status(200).json({
+                                "Error": "Something went wron trying to save this record"
+                            });
+                        }
+                    }).catch(err => {
+                        res.status(500).json({
+                            "Error": err
+                        });
+                    });
             }
             else {
-
+                helpers._showError(res, 500, "Fees configuration spec result is empty");
             }
         }
         _clearError();
@@ -50,11 +65,12 @@ exports._ComputeTransactionFees = async (req, res) => {
         const data = req.body;
         let Customers = data.Customers;
         let PaymentEntity = data.PaymentEntity;
-        let feesLength = _feesResult.length;
         let payCurrency = data.Currency;
         let foundFee = [];
         let payEntity = _checkFeeEntity(PaymentEntity.Type);
         let payLocale = data.CurrencyCountry === PaymentEntity.Country ? "LOCL" : "INTL";
+
+        let feesLength = _feesResult.length;
 
         if (feesLength > 0) {
             if (data.Amount >= 0) {
@@ -74,9 +90,7 @@ exports._ComputeTransactionFees = async (req, res) => {
                         }
                     }
                     if (foundFee.length > 0) {
-
-
-                        helpers._showError(res, 500, foundFee[0]["FeeId"]);
+                       
                     }
                     else {
                         helpers._showError(res, 500, "No payment template was found for this payload");
@@ -88,9 +102,10 @@ exports._ComputeTransactionFees = async (req, res) => {
             }
         }
         else {
-            helpers._showError(res, 500, "Please call the fees endpoint first.");
+            helpers._showError(res, 500, "Please call the fees end point before calling the compute endpoint");
         }
         _clearError();
+
     } catch (e) {
         helpers._showError(res, 500, e.message);
     }
