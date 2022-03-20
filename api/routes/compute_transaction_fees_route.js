cb8da4e0fefb15de65f3cd78b0be04aa4bdd4f88
ComputerGuy <vicmee76@hotmail.com>
2022-03-20 09:54:08 +0100

    -- change route name for computing transaction fee

--- api/routes/compute_transaction_fees_route.js
+++ api/routes/compute_transaction_fees_route.js
@@ -2,6 +2,6 @@ const express = require('express');
 const feesController = require("../controllers/fees_controller");
 const _router = express.Router();
 
-_router.post("/", feesController._ComputeTransactionFees);
+_router.post("/", feesController._computeTransactionFees);
 
 module.exports = _router;
